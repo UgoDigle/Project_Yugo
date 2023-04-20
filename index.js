f34bd64f1aea63f69770bc8ffd45b224a42cca59
UgoDigle <ugo.digle@gmail.com>
2023-04-20 16:37:50 +0200

    form à jour

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function search(event) {
+    event.preventDefault();
+    const searchValue=document.querySelector("#searchInput").value;
+    const apiUrl=`https://world.openfoodfacts.org/api/v0/product/${searchValue}.json?fields=generic_name,ingredients_text`;
+    window.location.replace("produit.html");
+    console.log(apiUrl);
+}
+
+document.querySelector("form[role=search]").addEventListener("submit", search);
 document.querySelector("#energie>td.quantite").textContent="123";
 document.querySelector("#matieres_grasses>td.quantite").textContent="456";
 document.querySelector("#acides_gras_satures>td.quantite").textContent="789";
@@ -31,9 +40,3 @@ let srcNutriscore = document.querySelector(".nutriscore").getAttribute("src");
 let srcNovascore = document.querySelector(".novascore").getAttribute("src");
 let srcEcoscore = document.querySelector(".ecoscore").getAttribute("src");
 
-function search(event) {
-    event.preventDefault();
-    const searchValue=document.querySelector("#searchInput").value;
-    const apiUrl=`https://world.openfoodfacts.org/api/v0/product/${searchValue}.json?fields=generic_name,ingredients_text`;
-    console.log(apiUrl);
-}
